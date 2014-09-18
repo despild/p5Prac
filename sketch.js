be873fa2539a9b4f7147584990f3e19b1b544227
Kihyeok Park <despild@gmail.com>
2014-09-18 15:04:30 +0900

    1

--- sketch.js
+++ sketch.js
@@ -1,9 +1,18 @@
+var rectX,rectY;
+
+
 function setup(){
 	createCanvas(400,400);
+	rectX = width/2;
+rectY = height/2;
+
 }
 
 function draw(){
 	background(255);
 	fill(255,0,0);
-	rect(100,100,200,200);
+	noStroke();
+	rectMode(CENTER);
+	rect(rectX,rectY,200,200);
 }
+
