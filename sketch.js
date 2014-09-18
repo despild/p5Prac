33e881f284fe4a013a0bf12ed36b943e6a881b31
Kihyeok Park <despild@gmail.com>
2014-09-18 16:59:30 +0900

    add movement reaction

--- sketch.js
+++ sketch.js
@@ -2,18 +2,51 @@ var rectX,rectY;
 var vibCount;
 var bVib;
 var vibVar = 10;
-
+var moveX, moveY;
+var bMove, bPower;
+var powerX,powerY;
 function setup(){
 	createCanvas(400,400);
 	rectX = width/2;
 	rectY = height/2;
 	bVib = false;
+	bMove = false;
+	moveX = 0;
+	moveY = 0;
 	vibCount = 0;
+	power = 0;
 }
 
 function draw(){
 	background(255);
-	fill(255,0,0);
+	if(bPower){
+		power++;
+	}else{
+		power = 0;
+	}
+	if(bMove){ 
+		rectX +=(moveX/Math.abs(moveX)*powerX*0.5);
+		rectY +=(moveY/Math.abs(moveX)*powerY*0.5);
+		if(rectX>width-100 || rectX < 100){
+			powerX = -powerX*0.5;
+			if(rectX > width-100){
+				rectX = width-100;
+			}
+			if(rectX < 100){
+				rectX = 100;
+			}
+		}
+		if(rectY>width-100 || rectY < 100){
+			powerY = -powerY*0.5;
+			if(rectY > height-100){
+				rectY = height-100;
+			}
+			if(rectY < 100){
+				rectY = 100;
+			}
+		}
+	}
+	fill(255,power,0);
 	noStroke();
 	rectMode(CENTER);
 	vibration();
@@ -22,10 +55,10 @@ function draw(){
 
 function vibration(){
 	if(bVib){
-		if(vibCount<60){
+		if(vibCount<=60){
 			vibVar = -vibVar;
-			rectX = width/2 + vibVar;
-			rectY = height/2 + vibVar;
+			rectX = rectX + vibVar;
+			rectY = rectY + vibVar;
 			vibCount++;
 		}else{
 			vibCount = 0;
@@ -34,10 +67,28 @@ function vibration(){
 		
 	}	
 }
-function mouseClicked(){
-	if(mouseX>width/2-100 && mouseX<width/2+100 && mouseY>height/2-100 && mouseY<height/2+100){
-		bVib = true;
+function rectMove(){
+	if(bMove){
+
 	}
 }
 
+function mousePressed(){
+	if(mouseX>rectX-100 && mouseX<rectX+100 && mouseY>rectY-100 && mouseY<rectY+100){
+		bVib = true;
+	}else{
+		bPower = true;
+		moveX =  rectX - mouseX;
+		moveY =  rectY - mouseY;
+	}
+}
+function mouseReleased(){
+	if(bPower){
+		powerX = power;
+		powerY = power;
+	bMove = true;
+}
+	bPower = false;
+
+}
 
