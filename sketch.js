31bb77780dc2d4213bc25197425ceaf5e11b8146
Kihyeok Park <despild@gmail.com>
2014-09-18 15:18:55 +0900

    add vibration

--- sketch.js
+++ sketch.js
@@ -1,11 +1,14 @@
 var rectX,rectY;
-
+var vibCount;
+var bVib;
+var vibVar = 10;
 
 function setup(){
 	createCanvas(400,400);
 	rectX = width/2;
-rectY = height/2;
-
+	rectY = height/2;
+	bVib = false;
+	vibCount = 0;
 }
 
 function draw(){
@@ -13,6 +16,28 @@ function draw(){
 	fill(255,0,0);
 	noStroke();
 	rectMode(CENTER);
+	vibration();
 	rect(rectX,rectY,200,200);
 }
+
+function vibration(){
+	if(bVib){
+		if(vibCount<60){
+			vibVar = -vibVar;
+			rectX = width/2 + vibVar;
+			rectY = height/2 + vibVar;
+			vibCount++;
+		}else{
+			vibCount = 0;
+			bVib = false;
+		}
+		
+	}	
+}
+function mouseClicked(){
+	if(mouseX>width/2-100 && mouseX<width/2+100 && mouseY>height/2-100 && mouseY<height/2+100){
+		bVib = true;
+	}
+}
+
 
